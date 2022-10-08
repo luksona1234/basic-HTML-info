0b65732c8bb60542618e3c9a7844c2371e38dcef
Luka <lukakhachvani0311@gmail.com>
2022-10-08 21:29:41 +0400

    n

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 let p = document.querySelector('.p')
-let p = document.querySelector('.p')
-let p = document.querySelector('.p')
-let p = document.querySelector('.p')
-let p = document.querySelector('.p')
-let p = document.querySelector('.p')
-let p = document.querySelector('.p')
-let p = document.querySelector('.p')
+let p1 = document.querySelector('.p1')
+let p2 = document.querySelector('.p2')
+let p3 = document.querySelector('.p3')
+let p4 = document.querySelector('.p4')
+let p5 = document.querySelector('.p5')
+let p6 = document.querySelector('.p6')
+let p7 = document.querySelector('.p7')
+let p8 = document.querySelector('.p8')
+let p9 = document.querySelector('.p9')
+let p10 = document.querySelector('.p10')
+let container = document.querySelector('.container')
+
+
 
